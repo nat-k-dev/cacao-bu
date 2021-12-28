9b33c2bde7a3adc080ea9cd7749e88a029cb7096
appalse <frontend-hh@yandex.ru>
2021-12-28 19:45:15 +0300

    add landing page containers and style them

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import Logo from "./components/logo/logo";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+
 import './App.css';
 
 function App() {
+
+  const theme = createTheme({
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      primary: {
+        main: '#0971f1',
+        darker: '#053e85',
+      },
+      neutral: {
+        main: '#FFC0CB',
+        contrastText: '#fff',
+      },
+    },
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        
+        <div className="app__logo">
+          <Logo />
+        </div>
+        <Container 
+            sx={{
+              width: '100vw',
+              height: '100vh',
+              backgroundColor: 'pink'
+            }}
+        >.</Container>
+        <Container 
+            sx={{
+              width: '100vw',
+              height: '100vh',
+              backgroundColor: 'lightblue'
+            }}
+        >.</Container>
+        <Container 
+            sx={{
+              width: '100vw',
+              height: '100vh',
+              backgroundColor: 'pink'
+            }}
+        >.</Container>
+      </div>
+    </ThemeProvider>
+
   );
 }
 
